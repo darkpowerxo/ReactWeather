5a177aeec7d81977d7255e9bf270704308219d48
Sam Abtahi <soroush731@gmail.com>
2024-07-10 13:42:26 -0400

    main functions
    Main functions:
    1) search
    2) forcast

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import CurrentWeather from "./components/currentWeather/currentWeather";
+import Forecast from "./components/forecast/forecast";
 import './App.css';
 
+const url = process.env.REACT_APP_WEATHER_URL;
+const api_key = process.env.REACT_APP_WEATHER_API_KEY;
+
+const weather_request= `${url}weather?q=${Location}&units=metric&appid=${api_key}`;
+const forecast_request= `${url}forecast?q=${Location}&units=metric&appid=${api_key}`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[Location,setLocation]=useState('');
+  const[WeatherData,setWeatherData]= useState(null);
+  const[ShowWeather, setShowWeather] = useState(false);
+  const[ForecastData,setForecastData]= useState(null);
+  const[showForecast, setShowForecast] = useState(false);
+  
+  const searchLocation = (event: { key: string; }) => {
+    if (event.key === 'Enter') {
+      axios.get(weather_request)
+      .then((response) => {
+        setWeatherData(response.data);
+      });
+      setShowWeather(true);
+    }
+  }
+
+  const getForecast = () => {
+    axios.get(forecast_request)
+        .then((response) => {
+          setForecastData(response.data);
+        });
+    setShowForecast(true);
+  };
+
+  return (  
+    <div className="app">
+      <div className="container">
+        <div className="search">
+          <input 
+          type="text" 
+          value={Location}
+          onChange={event=> setLocation(event.target.value)}
+          placeholder='Enter Location'
+          onKeyDown={searchLocation}
+          />
+          {WeatherData ?
+          <button className="getForecast" type="button" onClick={getForecast}>Get forecast</button>  
+          : null}
+      </div>
+      {ShowWeather && WeatherData && <CurrentWeather data={WeatherData} />}
+      {showForecast && ForecastData && <Forecast data={ForecastData} />}
     </div>
+  </div>
   );
 }
 
